5cb345de8d12340a7d4f8e5206287ad6e7944c11
Young-Min Baek <ymbaek@se.kaist.ac.kr>
2022-09-22 13:09:27 +0900

    initial commit

---
+++ database.js
@@ -0,0 +1,36 @@
+console.log('[database.js] start initialization')
+
+//node modules
+const csvToJson = require('convert-csv-to-json')
+
+const database = {
+    spots: [],
+    routes: [],
+    trips: []
+}
+
+
+Object.keys(database).forEach((key) => {
+    database[key] = [
+        ...database[key],
+        ...csvToJson.fieldDelimiter(',')
+        .getJsonFromCsv(`./sroum-data-in-csv/${key}.csv`)
+    ]
+
+    //숫자인 항목은 타입을 Number로 변형
+    if(database[key].length > 0){
+        const firstItem = database[key][0];
+
+        Object.keys(firstItem).forEach((itemKey) => {
+            if (database[key].every((item) => {
+                return /^-?\d+$/.test(item[itemKey])
+            })){
+                database[key].forEach((item) => {
+                    item[itemKey] = Number(item[itemKey])
+                })
+            }
+        })
+    }
+})
+
+module.exports = database
---
+++ index.js
@@ -0,0 +1,24 @@
+const database = require('./database')
+const { ApolloServer, gql } = require('apollo-server')
+const typeDefs = gql`
+    type Query {
+        spots: [Spot]
+    }
+    type Spot {
+        col1: String
+        col2: String
+        col3: String
+        col4: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        spots: () => database.spots
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen().then(({ url }) => {
+    console.log(`[index.js] Server ready at ${url}`)
+})
